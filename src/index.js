b48f9f9726c298fd1a720667698f83ee1dda1e5c
Michael <michael.persikov@gmail.com>
2024-01-26 14:28:30 +0200

    add: PG

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// import express from 'express';
-// const express = require('express');
-// const server = server.express();
-
-
-// const PORT = 4000;
-
-// server.listen(PORT, () => `Server is running on ${PORT}`);
