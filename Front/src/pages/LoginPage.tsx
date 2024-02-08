31dfe0c27f6b899c25ec562381a9b3e32678e7a7
m-persikov <michael.persikov@gmail.com>
2024-02-08 21:00:57 +0200

    Add: LoginPage style

--- Front/src/pages/LoginPage.tsx
+++ Front/src/pages/LoginPage.tsx
@@ -1,3 +1,12 @@
+import './LoginPage.css';
+
 export const LoginPage = () => {
-	return <div>LoginPage</div>;
+	return (
+		<div className="login_page_container">
+			<div className="login_page_from_container">
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
 };
