683bc24ab5b830927c34f263bebcb8f4664492f5
Michael <michael.persikov@gmail.com>
2024-02-15 11:12:38 +0200

    fix: HTMLProps to InputHTMLAttributes

--- Front/src/common/fields/inputs/Input/Input.tsx
+++ Front/src/common/fields/inputs/Input/Input.tsx
@@ -1,13 +1,14 @@
-import { FC, HTMLProps, useState } from 'react';
+import { FC, InputHTMLAttributes } from 'react';
 
 import styles from './Input.module.css';
 
-interface InputProps extends HTMLProps<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	isError?: boolean;
 	helperText?: string;
 }
 
 export const Input: FC<InputProps> = ({ isError = false, helperText, ...props }) => {
+	props.type = '';
 	return (
 		<div className={styles.input_container}>
 			<input className={`${styles.input} ${isError ? styles.input_error : ''}`} {...props} />
